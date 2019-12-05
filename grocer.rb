6d1fe3d3821d5b28bf86b5aeedc6ffdfa2959978
Zachary Jacobs <skatboyza124@hotmail.com>
2019-12-05 01:47:32 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -38,7 +38,7 @@ def apply_coupons(cart, coupons)
   index = 0 
    while index < coupons.length do
      cart_item = find_item_by_name_in_collection(coupons[index][:item],cart)
-   coupon_item_name = "#{coupons[index][:item]} W/COUPON "
+   coupon_item_name = "#{coupons[index][:item]} W/COUPON"
    cart_item_with_coupon = find_item_by_name_in_collection(coupon_item_name, cart)
      
      if cart_item && cart_item[:count] >= coupons[index][:num]
