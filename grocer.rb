770206ebf30cd7a8886dcb4182ef9e446812a4c6
Zachary Jacobs <skatboyza124@hotmail.com>
2019-12-05 01:18:09 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -55,6 +55,7 @@ def apply_coupons(cart, coupons)
            :clearance => cart_item[:clearance]
          }
          cart << cart_item_with_coupon
+         cart_item[index]
        
   end
   index += 1
