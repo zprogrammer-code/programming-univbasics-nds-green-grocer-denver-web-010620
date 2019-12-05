6338387eb04622a69c49a87437b0f1a47da0a6c8
Zachary Jacobs <skatboyza124@hotmail.com>
2019-12-05 00:47:10 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -17,7 +17,7 @@ def consolidate_cart(cart)
   new_cart = []
   index = 0
   while index < cart.length do 
-    new_cart_item = find_item_by_name_in_collection(coupon[index][:item],new_cart )
+    new_cart_item = find_item_by_name_in_collection(cart[index][:item],new_cart )
     
     if new_cart_item != nil
       new_cart_item[:count] += 1
